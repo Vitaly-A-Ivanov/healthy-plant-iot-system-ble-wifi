a9c2b79bd665bf6d306cbbfa0cd6cf9fc1bab575
Vitaly <viv55941@icloud.com>
2021-12-08 12:12:33 +0000

    code refactoring

--- healthy-plant-iot-system-wifi-ble.ino
+++ healthy-plant-iot-system-wifi-ble.ino
@@ -58,7 +58,7 @@ BLEServer* pServer = NULL;
 /****** WiFi and UBidots fields *****************************************/
 const char* WIFI_SSID = "Three_5G";
 const char* WIFI_PASSWORD = "Varvara2010";
-const char* UBIDOTS_TOKEN = "BBFF-zFr5hYpYnLlKINl15kl1kALvXonnfs"; 
+const char* UBIDOTS_TOKEN = "BBFF-zFr5hYpYnLlKINl15kl1kALvXonnfs";
 Ubidots ubidots(UBIDOTS_TOKEN, UBI_HTTP);
 /****** WiFi and UBidots fields *****************************************/
 
@@ -118,11 +118,13 @@ RTC_DATA_ATTR uint8_t oldDeviceConnected = false;
 
 /****** Deep Sleep fields *********************************/
 const uint8_t DEEP_SLEEP_TIME = 120; // in minutes
-const uint8_t SLEEP_DELAY_TIME = 15; // in minutes
+const uint8_t SLEEP_DELAY_TIME = 1; // in minutes
+/****** Deep Sleep fields *********************************/
 
-
-const unsigned long eventInterval = 1800000;
-unsigned long previousTime = 0;
+/****** Awake fields *********************************/
+const uint8_t AWAKE_INTERVAL = 1; // in minutes
+uint32_t previousTime = 0;
+/****** Awake fields *********************************/
 
 /*
 * Checks a plant moisture.
@@ -178,7 +180,6 @@ bool is_number(const std::string& s) {
 uint8_t checkIfProblem() {
   if (isMoistTooHigh || isMoistTooLow || isTempTooHigh || isTempTooLow
     || isHumidTooHigh || isHumidTooLow || isLightTooHigh || isLightTooLow) {
-    digitalWrite(LED_BUILTIN, HIGH);
     return true;
   }
   else
@@ -266,18 +267,11 @@ void sendBLENotification() {
 * Sends notifications via BLE to the client if any of the threshold are above the limit.
 * Checks if device connected or not to the server.
 */
-void sendToUbidots() {
-  uint16_t humid = humidityLevel;
-  int temp = temperatureLevel;
-  uint16_t light = lightLevel;
-  uint16_t moist = moistureLevel;
-  
-
-
-  ubidots.add("Temperature", temp); 
-  ubidots.add("Humidity", humid);
-  ubidots.add("Soil Moisture", moist);
-  ubidots.add("Light Intensity", light);
+void sendToUbidots(int t, uint16_t m, uint16_t h, uint16_t l) {
+  ubidots.add("Temperature", t);
+  ubidots.add("Humidity", h);
+  ubidots.add("Soil Moisture", m);
+  ubidots.add("Light Intensity", l);
 
   bool bufferSent = false;
   bufferSent = ubidots.send();  // Will send data to a device label that matches the device Id
@@ -285,10 +279,12 @@ void sendToUbidots() {
   if (bufferSent) {
     // Do something if values were sent properly
     Serial.println("Values sent by the device");
-  } else {
-     Serial.println("Values did not send by the device");
-    }
+  }
+  else {
+    Serial.println("Values did not send by the device");
+  }
 }
+
 /*
 * Sends the unit to the deep sleep.
 * Provides possibility to set the time to awake from sleep.
@@ -296,7 +292,7 @@ void sendToUbidots() {
 void goToDeepSleep() {
   Serial.println("Going to sleep now for " + String(DEEP_SLEEP_TIME) + " Minutes");
   esp_sleep_enable_timer_wakeup(DEEP_SLEEP_TIME * 600000);
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,1);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, 1);
   Serial.println("Slepping.....");
   delay(1000);
 
@@ -334,137 +330,6 @@ void print_wakeup_reason() {
 }
 
 /*
-* Reads all sensors. Compare results with their thresholds.
-* Prints the message to the monitor if readings above the threshold
-*/
-void readSensors() {
-  // read the moisture
-  Serial.println(F(""));
-  uint16_t moisture = getMoisture();
-  if (isnan(moisture)) {
-    Serial.println(F("Failed to read from moisture sensor!"));
-    isMoistSensorFault = true;
-  }
-  else {
-    moistureLevel = moisture;
-    Serial.print(moisture);
-
-    //checks against it's thresholds
-    if (moisture < minMoist) {
-      Serial.println(F(" - Time to water your plant!"));
-      isMoistTooLow = true;
-    }
-    else {
-      isMoistTooLow = false;
-    }
-    if (moisture > maxMoist) {
-      Serial.println(F(" - Too much water in your plant!"));
-      isMoistTooLow = true;
-    }
-    else {
-      isMoistTooHigh = false;
-    }
-    if (moisture >= minMoist && moisture <= maxMoist) {
-      isMoistTooLow = false;
-      isMoistTooHigh = false;
-      Serial.println(F(" - Doesn't need watering!"));
-    }
-  }
-  // read temperature
-  int temp = getTemperature();
-
-  if (isnan(temp)) {
-    Serial.println(F("Failed to read a temperature!"));
-    isTempSensorFault = true;
-  }
-  else {
-    temperatureLevel = temp;
-    Serial.print(temp);
-    Serial.print(F(" °C"));
-
-    //checks against it's thresholds
-    if (temp < minTemp) {
-      Serial.println(F(" - Temperaure is to low!"));
-      isTempTooLow = true;
-    }
-    else {
-      isTempTooLow = false;
-    }
-    if (temp > maxTemp) {
-      Serial.println(F(" - Temperaure is too high!"));
-      isTempTooHigh = true;
-    }
-    else {
-      isTempTooHigh = false;
-    }
-    if (temp >= minTemp && temp <= maxTemp) {
-      isTempTooLow = false;
-      isTempTooHigh = false;
-      Serial.println(F(" - Temperature is ok!"));
-    }
-  }
-
-  // read humidity
-  uint16_t humid = getHumidity();
-  if (isnan(humid)) {
-    Serial.println(F("Failed to read a humidity!"));
-    isHumidSensorFault = true;
-  }
-  else {
-    humidityLevel = humid;
-    Serial.print(humid);
-    Serial.print(F(" %"));
-
-    //checks against it's thresholds
-    if (humid > maxHumid) {
-      Serial.println(F(" - Humidity is to high!"));
-      isHumidTooHigh = true;
-    }
-    else {
-      isHumidTooHigh = false;
-    }
-    if (humid < minHumid) {
-      Serial.println(F(" - Humidity is too low!"));
-      isHumidTooLow = true;
-    }
-    else {
-      isHumidTooLow = false;
-    }
-    if (humid >= minHumid && humid <= maxHumid) {
-      isHumidTooLow = false;
-      isHumidTooHigh = false;
-      Serial.println(F(" - Humidity is ok!"));
-    }
-  }
-
-  // read light intensity
-  uint16_t light = getLightIntensity();
-  if (isnan(light)) {
-    isLightSensorFault = true;
-    Serial.println(F("Failed to read a humidity!"));
-  }
-  else {
-    lightLevel = light;
-    Serial.print(light);
-
-    //checks against it's thresholds
-    if (light > maxLight) {
-      isLightTooHigh = true;
-      Serial.println(F(" - Light is to high!"));
-    }
-    if (light < minLight) {
-      isLightTooLow = true;
-      Serial.println(F(" - Light is to low!"));
-    }
-    if (light <= maxLight && light >= minLight) {
-      isLightTooLow = false;
-      isLightTooHigh = false;
-      Serial.println(F(" - Light is ok!"));
-    }
-  }
-}
-
-/*
  * Initialize WiFi connection with the board.
 //*/
 //void initWiFi() {
@@ -477,6 +342,110 @@ void readSensors() {
 //  }
 //  Serial.println(WiFi.localIP());
 //}
+
+
+void printReadingsToMonitor(int t, uint16_t m, uint16_t h, uint16_t l) {
+  // moisture
+  Serial.println(F(""));
+  uint16_t moisture = m;
+  if (isnan(moisture)) Serial.println(F("Failed to read from moisture sensor!"));
+  else {
+    moistureLevel = moisture;
+    Serial.print(moisture);
+    //checks against it's thresholds
+    if (moisture < minMoist) Serial.println(F(" - Time to water your plant!"));
+    if (moisture > maxMoist) Serial.println(F(" - Too much water in your plant!"));
+    if (moisture >= minMoist && moisture <= maxMoist) Serial.println(F(" - Doesn't need watering!"));
+  }
+
+  //temperature
+  int temp = t;
+  if (isnan(temp)) Serial.println(F("Failed to read a temperature!"));
+  else {
+    temperatureLevel = temp;
+    Serial.print(temp);
+    Serial.print(F(" °C"));
+    //checks against it's thresholds
+    if (temp < minTemp) Serial.println(F(" - Temperaure is to low!"));
+    if (temp > maxTemp) Serial.println(F(" - Temperaure is too high!"));
+    if (temp >= minTemp && temp <= maxTemp) Serial.println(F(" - Temperature is ok!"));
+  }
+
+  // humidity
+  uint16_t humid = h;
+  if (isnan(humid)) Serial.println(F("Failed to read a humidity!"));
+  else {
+    humidityLevel = humid;
+    Serial.print(humid);
+    Serial.print(F(" %"));
+    //checks against it's thresholds
+    if (humid > maxHumid) Serial.println(F(" - Humidity is to high!"));
+    if (humid < minHumid) Serial.println(F(" - Humidity is too low!"));
+    if (humid >= minHumid && humid <= maxHumid) Serial.println(F(" - Humidity is ok!"));
+  }
+
+  // light intensity
+  uint16_t light = l;
+  if (isnan(light))  Serial.println(F("Failed to read a humidity!"));
+  else {
+    lightLevel = light;
+    Serial.print(light);
+    //checks against it's thresholds
+    if (light > maxLight) Serial.println(F(" - Light is to high!"));
+    if (light < minLight) Serial.println(F(" - Light is to low!"));
+    if (light <= maxLight && light >= minLight) Serial.println(F(" - Light is ok!"));
+  }
+}
+
+void checkAgainstThresholds(int t, uint16_t m, uint16_t h, uint16_t l) {
+  // moisture
+  if (isnan(m)) {
+    isMoistSensorFault = true;
+  }
+  else {
+    m < minMoist ? isMoistTooLow = true : isMoistTooLow = false;
+    m > maxMoist ? isMoistTooLow = true : isMoistTooHigh = false;
+    if (m >= minMoist && m <= maxMoist) {
+      isMoistTooLow = false;
+      isMoistTooHigh = false;
+    }
+  }
+  //  temperature
+  if (isnan(t)) {
+    isTempSensorFault = true;
+  }
+  else {
+    t < minTemp ? isTempTooLow = true : isTempTooLow = false;
+    t > maxTemp ? isTempTooHigh = true : isTempTooHigh = false;
+    if (t >= minTemp && t <= maxTemp) {
+      isTempTooLow = false;
+      isTempTooHigh = false;
+    }
+  }
+
+  // humidity
+  if (isnan(h)) isHumidSensorFault = true;
+  else {
+    h > maxHumid ? isHumidTooHigh = true : isHumidTooHigh = false;
+    h < minHumid ? isHumidTooLow = true : isHumidTooLow = false;
+    if (h >= minHumid && h <= maxHumid) {
+      isHumidTooLow = false;
+      isHumidTooHigh = false;
+    }
+  }
+
+  // light intensity
+  if (isnan(l)) isLightSensorFault = true;
+  else {
+    l > maxLight ? isLightTooHigh = true : isLightTooHigh = false;
+    l < minLight ? isLightTooLow = true : isLightTooLow = false;
+    if (l <= maxLight && l >= minLight) {
+      isLightTooLow = false;
+      isLightTooHigh = false;
+    }
+  }
+
+}
 
 /*
 * The callback function that handles receiving data being sent from the client(phone) and Bluetooth connection status.
@@ -503,13 +472,13 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       if (is_number(pCharacteristic->getValue().c_str())) {
         uint16_t i = atoi((pCharacteristic->getValue().c_str()));
         minMoist = i;
-        readSensors();
-      
+        //        int *data = getAllSensorsValues();
+
 
         if (checkIfProblem()) {
           sendBLENotification();
         }
-        
+
       }
       else {
         Serial.println(F("not a number"));
@@ -520,7 +489,7 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       if (is_number(pCharacteristic->getValue().c_str())) {
         uint16_t i = atoi((pCharacteristic->getValue().c_str()));
         maxMoist = i;
-                readSensors();
+        //        int *data = getAllSensorsValues();
 
         if (checkIfProblem()) {
           sendBLENotification();
@@ -534,12 +503,12 @@ class MyCallbacks : public BLECharacteristicCallbacks {
     if (tempMinCharID.equals(pCharacteristic->getUUID())) {
       if (is_number(pCharacteristic->getValue().c_str())) {
         int i = atoi((pCharacteristic->getValue().c_str()));
-         minTemp = i;
-         int ml = getTemperature();
-         if(ml < minTemp {
+        minTemp = i;
+        int ml = getTemperature();
+        if (ml < minTemp) {
           isTempTooLow = true;
           sendBLENotification();
-          }
+        }
       }
       else {
         Serial.println(F("not a number"));
@@ -550,8 +519,8 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       if (is_number(pCharacteristic->getValue().c_str())) {
         int i = atoi((pCharacteristic->getValue().c_str()));
         maxTemp = i;
-                readSensors();
-                  delay(500);
+        //         int *data = getAllSensorsValues();
+        delay(500);
 
         if (checkIfProblem()) {
           sendBLENotification();
@@ -566,8 +535,8 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       if (is_number(pCharacteristic->getValue().c_str())) {
         uint16_t i = atoi((pCharacteristic->getValue().c_str()));
         minHumid = i;
-                readSensors();
-                  delay(500);
+        //        int *data = getAllSensorsValues();
+        delay(500);
 
         if (checkIfProblem()) {
           sendBLENotification();
@@ -582,8 +551,8 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       if (is_number(pCharacteristic->getValue().c_str())) {
         uint16_t i = atoi((pCharacteristic->getValue().c_str()));
         maxHumid = i;
-                readSensors();
-                  delay(500);
+        //       int *data = getAllSensorsValues();
+        delay(500);
 
         if (checkIfProblem()) {
           sendBLENotification();
@@ -598,8 +567,8 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       if (is_number(pCharacteristic->getValue().c_str())) {
         uint16_t i = atoi((pCharacteristic->getValue().c_str()));
         minLight = i;
-                readSensors();
-        sendToUbidots();
+        //        int *data = getAllSensorsValues();
+        //        sendToUbidots(data);
         if (checkIfProblem()) {
           sendBLENotification();
         }
@@ -613,7 +582,7 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       if (is_number(pCharacteristic->getValue().c_str())) {
         uint16_t i = atoi((pCharacteristic->getValue().c_str()));
         maxLight = i;
-                readSensors();
+        //         int *data = getAllSensorsValues();
 
         if (checkIfProblem()) {
           sendBLENotification();
@@ -728,18 +697,18 @@ void setup() {
   pinMode(5, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-//   WiFi setup
-//   initWiFi();
-//   Serial.print("RRSI: ");
-//   Serial.println(WiFi.RSSI());
+  //   WiFi setup
+  //   initWiFi();
+  //   Serial.print("RRSI: ");
+  //   Serial.println(WiFi.RSSI());
 
-// Ubibot setup
+  // Ubibot setup
 
   ubidots.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
-//  ubidots.setDebug(true);
+  //  ubidots.setDebug(true);
 
 
-  // temperature and humidity sensor setup //
+    // temperature and humidity sensor setup //
   dht.begin();
 
   // Light sensor setup //
@@ -871,54 +840,73 @@ void setup() {
   BLEDevice::startAdvertising();
 
 
-//  print_wakeup_reason();
+  //  print_wakeup_reason();
 }
 
 void loop() {
-       unsigned long currentTime = millis();
-       digitalWrite(5, HIGH);
-       readSensors();
-       sendToUbidots();
-        
-        if (checkIfProblem()) {
-          sendBLENotification();
-        }
-        else {
-          digitalWrite(LED_BUILTIN, LOW);
-          Serial.println(F(""));
-          Serial.print(F("Everything is ok. Going to sleep in a "));
-          Serial.print(SLEEP_DELAY_TIME);
-          Serial.println(F(" minutes"));
-          
-              delay(SLEEP_DELAY_TIME * 60000); 
-              Serial.println(F(""));
-              Serial.println(F("Reading all sensors again before sleep..."));
-              readSensors();
-              sendToUbidots();
-              if (checkIfProblem()) {
-                Serial.println(F(""));
-                Serial.println(F("Sleep abandoned! Needs your attention!"));
-                sendBLENotification();
-              }
-              else {
-                Serial.println(F(""));
-                Serial.println(F("No changes..."));
-                digitalWrite(5, LOW);
-                goToDeepSleep();
-              }
-        }
-        
- 
+  uint8_t sleepAbandonned = false;
+  uint32_t currentTime = millis();
+  digitalWrite(5, HIGH);
+  int temp = getTemperature();
+  uint16_t moist = getMoisture();
+  uint16_t humid = getHumidity();
+  uint16_t lux = getLightIntensity();
+  printReadingsToMonitor(temp, moist, humid, lux);
+  checkAgainstThresholds(temp, moist, humid, lux);
+  sendToUbidots(temp, moist, humid, lux);
+  if (checkIfProblem()) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    sendBLENotification();
+    //    sendEmailNotification();
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println(F(""));
+    Serial.print(F("Everything is ok. Going to sleep in a "));
+    Serial.print(SLEEP_DELAY_TIME);
+    Serial.println(F(" minutes"));
 
-    while (currentTime - previousTime <= eventInterval) {
-          currentTime = millis();
-          if(!checkIfProblem()) {
-            break;
-          }
+    while (currentTime - previousTime <= SLEEP_DELAY_TIME * 60000) {
+      currentTime = millis();
     }
     previousTime = currentTime;
-    delay(1000);
-   
 
+    Serial.println(F(""));
+    Serial.println(F("Reading all sensors again before sleep..."));
+    int t = getTemperature();
+    uint16_t m = getMoisture();
+    uint16_t h = getHumidity();
+    uint16_t l = getLightIntensity();
+    printReadingsToMonitor(t, m, h, l);
+    checkAgainstThresholds(t, m, h, l);
+    sendToUbidots(t, m, h, l);
+    if (checkIfProblem()) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println(F(""));
+      Serial.println(F("Sleep abandoned! Needs your attention!"));
+      sendBLENotification();
+      //    sendEmailNotification();
+    }
+    else {
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.println(F(""));
+      Serial.println(F("No changes..."));
+      digitalWrite(5, LOW);
+      goToDeepSleep();
+    }
+  }
 
+  Serial.println(F(""));
+  Serial.print(F("Staying awake for "));
+  Serial.print(AWAKE_INTERVAL);
+  Serial.println(F(" minutes"));
+
+  while (currentTime - previousTime <= AWAKE_INTERVAL * 60000) {
+    currentTime = millis();
+    if (!checkIfProblem()) {
+      break;
+    }
+  }
+  previousTime = currentTime;
+  delay(1000);
 }
